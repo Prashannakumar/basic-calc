9568e34b70bd1034a1d69bed49dd5383935070d6
prashannakumar <prashannakumar.a@aspirantlabs.com>
2023-05-01 11:54:30 +0530

    added basic calc, bootstrap, firebase init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'basic-calc';
+  
+  opVal:any = 0;
+  mathNum:any[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getValues(ip:any){
+    this.mathNum.push(ip);
+    this.opVal = this.mathNum.join('');
+  }
+
+   doEval(){
+    let result = eval(this.opVal);
+    this.opVal = result;
+  }
+
+  reset(){
+    this.opVal=0;
+    this.mathNum =[];
+  }
 }
